54c41cd1c08ae55f7481dcadae4a17ab7143a40b
Nokuphiwa <shangenokuphiwa683@gmail.com>
2026-07-24 08:45:46 +0200

    navReact

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import styles from './Navbar.module.css'
+import styles from "./Navbar.module.css";
 
 export const Navbar = () => {
   return (
     <div className={styles.navTitle}>
-        <h1>Link Vault</h1>
+      <h1>Link Vault</h1>
     </div>
-  )
-}
+  );
+};
